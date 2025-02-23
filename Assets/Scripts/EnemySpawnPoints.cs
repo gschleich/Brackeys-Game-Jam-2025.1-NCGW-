53b9ae25d11792edd92d1b0ebb849d3bda20775f
Garrett Schleich <bluegarrett13@gmail.com>
2025-02-23 02:57:52 -0700

    Fixed Build Errors

--- Assets/Scripts/EnemySpawnPoints.cs
+++ Assets/Scripts/EnemySpawnPoints.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor; // Add this line to include the UnityEditor namespace for the Handles class
+#endif
+
 public class EnemySpawnPoints : MonoBehaviour
 {
     [SerializeField] private Transform[] spawnPoints; // Array of spawn point transforms
@@ -20,7 +24,10 @@ public class EnemySpawnPoints : MonoBehaviour
             if (spawnPoints[i] != null)
             {
                 Gizmos.DrawSphere(spawnPoints[i].position, gizmoSize);
+                
+                #if UNITY_EDITOR
                 UnityEditor.Handles.Label(spawnPoints[i].position + Vector3.up * 0.2f, $"Spawn{i + 1}");
+                #endif
             }
         }
     }
